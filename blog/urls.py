92b12654721e45bf4459fccee231b416f0fac38b
Artem Melnyk <temmar1991@gmail.com>
2017-09-01 17:24:10 +0300

    Add post form and bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url
 from . import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
 
   url(r'^$', views.post_list, name='post_list'),
  # url(r'^post/(?P<pk>[0-9]+)/$'), views.post_detail, name='post_detail'),
   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+  url(r'^post/new/$', views.post_new, name='post_new'),
+  url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+
+  url(r'^bootstap$', TemplateView.as_view(template_name='blog/bootstap_demo1.html')),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
             ],
         },
     },
+
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
